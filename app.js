337d9f672aaed289317ec58753d86708d7066de8
Ryan Park <ryan.developer99@gmail.com>
2021-04-14 12:37:10 +0900

    Fourth commit: move people element to data.js file

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-import { random } from "./utils/data.js";
+import { random, people } from "./utils/data.js";
 console.log(random);
-
-const people = [
-  { name: "john", job: "fe developer" },
-  { name: "susan", job: "be developer" },
-  { name: "anna", job: "devop developer" },
-];
+console.log(people);
 
 const container = document.querySelector(".container");
 const btn = document.querySelector(".btn");
--- utils/data.js
+++ utils/data.js
@@ -1,1 +1,7 @@
 export const random = "Random Value";
+
+export const people = [
+  { name: "john", job: "fe developer" },
+  { name: "susan", job: "be developer" },
+  { name: "anna", job: "devop developer" },
+];
